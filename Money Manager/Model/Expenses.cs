a05b1dcc8116b67aa226543dec87902e1fdec792
Nijat Muradov <live4ever4@gmail.com>
2021-12-08 14:49:20 +0400

    Добавьте файлы проекта.

---
+++ Money Manager/Model/Expenses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Manager.Model
+{
+    internal class Expenses //Расходы, затраты
+    {
+        private decimal balance;            //общий бюджет
+        private decimal food;               //еда
+        private decimal health;             //здоровье 
+        private decimal clothes;            //одежда
+        private decimal entertainment;      //развлечения
+        private decimal transport_costs;    //расходы на транспорт
+        private decimal rental_of_property; //аренда жилья
+        private decimal municipal_services; //коммунальные услуги
+        private decimal debt;               //долги;
+        private decimal other;              //другое
+
+        public decimal Balance  
+        {
+            get { return balance; }
+            set 
+            {
+                if (balance > 0)
+                { 
+                    balance = value; 
+                }
+                else
+                {
+                    balance = 0;
+                }
+            }
+        }
+
+       
+    }
+}
---
+++ Money Manager/Program.cs
@@ -0,0 +1,17 @@
+using Money_Manager.View;
+
+namespace Money_Manager
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
